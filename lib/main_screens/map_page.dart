81cd98657be62c7cf6c9151bb89a9001fc1e3089
Advait Bharadwaj <abharadwaj@umass.edu>
2024-11-09 13:26:46 -0500

    added a bunch of help phone locations.

--- lib/main_screens/map_page.dart
+++ lib/main_screens/map_page.dart
@@ -107,36 +107,85 @@ class _MapPageState extends State<MapPage> {
   ];
 
   // helpLines
-  static const LatLng helpline1 = LatLng(42.382830, -72.520950); // 358 N Pleasant St.
-  static const LatLng helpline2 = LatLng(42.3895, -72.5198); // Baker House
-  static const LatLng helpline3 = LatLng(42.340382, -72.496819); // Bartlett- WestSide
-  //static const LatLng helpline4 = LatLng(42., -72.); // Berkshire DC
-  // static const LatLng helpline5 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline6 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline7 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline8 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline9 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline10 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline11 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline12 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline13 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline14 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline15 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline16 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline17 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline18 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline19 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline20 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline21 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline22 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline23 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline24 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline25 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline26 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline27 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline28 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline29 = LatLng(42., -72.); // Baker House
-  // static const LatLng helpline30 = LatLng(42., -72.); // Baker House
+  static const LatLng helpline1 = LatLng(42.3828003, -72.5233708); // 358 N Pleasant St.
+  static const LatLng helpline2 = LatLng(42.3895591, -72.5199808); // Baker House
+  static const LatLng helpline3 = LatLng(42.3879676, -72.531249); // Bartlett – West Side
+  static const LatLng helpline4 = LatLng(42.3819085, -72.5325428); // Berkshire DC – South Side- Malcom X
+  static const LatLng helpline5 = LatLng(42.3855426, -72.5315166); // Berkshire House – South Side
+  static const LatLng helpline6 = LatLng(42.3880345, -72.5338728); // Birch
+  static const LatLng helpline7 = LatLng(42.386225, -72.5353479); // Boyden Field – Tunnel Entrance
+  static const LatLng helpline8 = LatLng(42.3894571, -72.5241368); // Brett House
+  static const LatLng helpline9 = LatLng(42.3898921, -72.5235905); // Brooks House
+  static const LatLng helpline10 = LatLng(42.3975058, -72.5255143); // Brown House
+  static const LatLng helpline11 = LatLng(42.3940649, -72.5241485); // Burma Trail – Orchard Hill
+  static const LatLng helpline12 = LatLng(42.3790682, -72.5366818); // Bus Shelter B – Stadium Rd.
+  static const LatLng helpline13 = LatLng(42.395447, -72.5140719); // Bus Shelter C – Olympia Dr.
+  static const LatLng helpline14 = LatLng(42.392177, -72.5376411); // Bus Shelter D – Lot 12 behind Parking Services
+  static const LatLng helpline15 = LatLng(42.3798203, -72.5299287); // Bus Shelter E – Mass Ave / Sunset
+  static const LatLng helpline16 = LatLng(42.388535, -72.520758); // Butterfield House
+  static const LatLng helpline17 = LatLng(42.3912006, -72.5316747); // Campus Center Parking Garage – Loading Dock
+  static const LatLng helpline18 = LatLng(42.3900109, -72.5253501); // Campus Pond – West Side
+  static const LatLng helpline19 = LatLng(42.3810958, -72.5324821); // Cance House – Front (North Side)
+  static const LatLng helpline20 = LatLng(42.3974806, -72.5243902); // Cashin House
+  static const LatLng helpline21 = LatLng(42.3893424, -72.5216664); // Chadbourne House
+  static const LatLng helpline22 = LatLng(42.3883976, -72.535292); // Champions Center S. West – Back Door
+  static const LatLng helpline23 = LatLng(42.3920879, -72.5328105); // Chenoweth
+  static const LatLng helpline24 = LatLng(42.395155, -72.5320253); // Computer Science – North Entrance
+  static const LatLng helpline25 = LatLng(42.395155, -72.5320253); // Computer Science – Southwest Corner
+  static const LatLng helpline26 = LatLng(42.3258149, -72.5333646); // Conte Loading Dock
+  static const LatLng helpline27 = LatLng(42.3836719, -72.5324764); // Coolidge Tower
+  static const LatLng helpline28 = LatLng(42.3940168, -72.5276155); // Crabtree House
+  static const LatLng helpline29 = LatLng(42.383376, -72.5314189); // Crampton House
+  static const LatLng helpline30 = LatLng(42.3869766, -72.5308179); // Curry Hicks
+  static const LatLng helpline31 = LatLng(42.3921189, -72.5222402); // Dickinson House
+  static const LatLng helpline32 = LatLng(42.3923751, -72.529093); // Draper Annex – East Side
+  static const LatLng helpline33 = LatLng(42.3906299, -72.5251164); // Durfee – North Side
+  static const LatLng helpline34 = LatLng(42.3953759, -72.527793); // Dwight House
+  static const LatLng helpline35 = LatLng(42.3942554, -72.5318593); // ELAB II – North End
+  static const LatLng helpline36 = LatLng(42.3942651, -72.531796); // ELAB II – South End
+  static const LatLng helpline37 = LatLng(42.3781768, -72.5284888); // Elm East
+  static const LatLng helpline38 = LatLng(42.378375, -72.524991); // Elm West
+  static const LatLng helpline39 = LatLng(42.3834682, -72.5339336); // Emerson House
+  static const LatLng helpline40 = LatLng(42.3916293, -72.5211382); // Field House
+  static const LatLng helpline41 = LatLng(42.3894871, -72.5228677); // Franklin DC ATM
+  static const LatLng helpline42 = LatLng(42.3982195, -72.5290648); // Furcolo
+  static const LatLng helpline43 = LatLng(42.3848695, -72.5245028); // Gordon Hall
+  static const LatLng helpline44 = LatLng(42.3886711, -72.5318386); // Goodell – Upper Entrance
+  static const LatLng helpline45 = LatLng(42.3874802, -72.5235953); // Gorman House
+  static const LatLng helpline46 = LatLng(42.3922361, -72.5213507); // Grayson House
+  static const LatLng helpline47 = LatLng(42.3899945, -72.5218677); // Greenough House
+  static const LatLng helpline48 = LatLng(42.3838499, -72.5330939); // Hampshire DC ATM
+  static const LatLng helpline49 = LatLng(42.3950718, -72.5290643); // Hamlin House
+  static const LatLng helpline50 = LatLng(42.3903742, -72.5242077); // Health Services – Main Entrance
+  static const LatLng helpline51 = LatLng(42.387095, -72.526445); // Herter – Haigis Mall @ Bus Stop
+  static const LatLng helpline52 = LatLng(42.390946, -72.545321); // ILC East – Second Floor
+  // static const LatLng helpline53 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline54 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline55 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline56 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline57 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline58 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline59 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline60 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline61 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline62 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline63 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline64 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline65 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline66 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline67 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline68 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline69 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline70 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline71 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline72 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline73 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline74 = LatLng(42., -72.); // Baker House
+  // static const LatLng helpline75 = LatLng(42., -72.); // Baker House
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
